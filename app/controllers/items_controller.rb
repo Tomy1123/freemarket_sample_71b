ab8a69f3db63c2a1a21585e41637c0ba04c2b635
Tomy1123 <rifinyu872@ahk.jp>
2020-03-25 14:32:35 +0900

    ビュー３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   def new
     @product = Categorie.where(id: 14..19)
+    @products = Categorie.where(id: 29..31)
     @parent = Categorie.where(id: 1..13)
 
   end
