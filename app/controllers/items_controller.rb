6500af5fba197d8f6621ae25aa3314254e56622f
Tomy1123 <rifinyu872@ahk.jp>
2020-03-26 20:20:54 +0900

    form_with

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,14 @@
 class ItemsController < ApplicationController
   def new
+    @item = Item.new
     @product = Categorie.where(id: 14..19)
     @products = Categorie.where(id: 29..31)
     @parent = Categorie.where(id: 1..13)
-
   end
   
   def show
+    @item = Item.find_by(id: params[:id])
+    @categorie = Categorie.find_by(id: @item.categorie_id)
   end
 
 end
