3fbd1f17b0f5826d576018abb293d94744d80c2c
nakashunn <naka1215b@gmail.com>
2020-03-26 15:47:30 +0900

    商品詳細　ビュー　途中

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'items/new'
   root 'sell#index'
   resources :users, only: [:show]
-  resources :items, only: [:new]
+  resources :items, only: [:new, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
