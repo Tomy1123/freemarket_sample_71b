ecebe1ab647a184ac4ca05b468efffce4f3050a8
toshiya-shimizu <email@example.com>
2020-03-27 21:35:23 +0900

    商品購入ビュー

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_03_27_071905) do
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
