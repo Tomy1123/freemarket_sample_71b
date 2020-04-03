1e5e27d1e3b94e1de14756e3c5ca568efec09843
toshiya-shimizu <email@example.com>
2020-04-03 17:00:01 +0900

    商品購入機能②

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_31_070307) do
+ActiveRecord::Schema.define(version: 2020_03_31_093013) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
