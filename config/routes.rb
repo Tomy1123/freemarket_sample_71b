9a0a9694f2a0e558e1772ef22f224f226e8d3e81
nakashunn <naka1215b@gmail.com>
2020-04-09 11:49:20 +0900

    pay.jp 実装１

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,33 @@
 Rails.application.routes.draw do
+  get 'purchase/index'
+  get 'purchase/done'
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
   get 'items/new'
   root 'sell#index'
   resources :users, only: [:show,:new]
-  resources :cards, only: [:new, :show, :destroy] do
-  end
   resources :items do
     collection do
       
       get "buy"
     end
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  resources :card, only: [:new, :show] do
+    collection do
+      post 'show', to: 'card#show'
+      post 'pay', to: 'card#pay'
+      post 'delete', to: 'card#delete'
+    end
+  end
+  resources :purchase, only: [:index] do
+    collection do
+      get 'index', to: 'purchase#index'
+      post 'pay', to: 'purchase#pay'
+      get 'done', to: 'purchase#done'
+    end
 end
+end
+
