9ff302812420bf8e5ee91919299d51a295443bf7
Tomy1123 <rifinyu872@ahk.jp>
2020-04-11 15:01:12 +0900

    sold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_060648) do
+ActiveRecord::Schema.define(version: 2020_04_11_050023) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_060648) do
 
   create_table "item_images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "item_id"
-    t.string "image_url", null: false
+    t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -46,20 +46,21 @@ ActiveRecord::Schema.define(version: 2020_04_06_060648) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
-    t.string "brand", null: false
-    t.integer "delivery_days", null: false
-    t.integer "delivery_area", null: false
+    t.string "brand"
+    t.integer "delivery_days"
+    t.integer "delivery_area"
     t.integer "user_id"
+    t.integer "purchaser_id"
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "first_name", null: false
-    t.string "family_name", null: false
-    t.string "first_name_kana", null: false
-    t.string "family_name_kana", null: false
-    t.string "birth_year", null: false
-    t.string "birth_month", null: false
-    t.string "birth_day", null: false
+    t.string "first_name"
+    t.string "family_name"
+    t.string "first_name_kana"
+    t.string "family_name_kana"
+    t.date "birth_year"
+    t.date "birth_month"
+    t.date "birth_day"
     t.text "introduction"
     t.string "avatar"
     t.bigint "user_id"
